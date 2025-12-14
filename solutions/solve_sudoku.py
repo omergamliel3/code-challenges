dcfe76f94a931bb9d6ea9ad1eceabf7545a9084c
Omer Gamliel <omergamliel3@gmail.com>
2025-12-15 01:27:22 +0200

    finish solve sudoku

--- solutions/solve_sudoku.py
+++ solutions/solve_sudoku.py
@@ -17,25 +17,106 @@ Constraints:
 """
 
 
-def solveSudoku(board: list[list[str]]) -> None:
-    pass
+from typing import Optional
 
 
-# todo: implement
-def printSudokuBoard(board: list[list[str]]) -> None:
-    for row_start in range(0, 9, 3):
-        for col_start in range(0, 9, 3):
-            for r in range(row_start, row_start + 3):
-                for c in range(col_start, col_start + 3):
-                    cell = board[r][c]
-                    print(cell)
+Board = list[list[str]]
+DIGITS = set("123456789")
+
+
+class Cell:
+    row: int
+    col: int
+
+    def __init__(self, row: int, col: int) -> None:
+        self.row = row
+        self.col = col
+
+
+def get_digist() -> set[str]:
+    return DIGITS.copy()
+
+
+def is_board_completed(board: Board) -> bool:
+    for row in board:
+        for cell in row:
+            if is_cell_empty(cell):
+                return False
+
+    return True
+
+
+def is_cell_empty(cell: str) -> bool:
+    return cell == '.'
+
+
+def get_candidates(board: Board, cell: Cell) -> set[str]:
+    row = cell.row
+    col = cell.col
+
+    if not is_cell_empty(board[row][col]):
+        return set()
+
+    used = set[str]()
+
+    # Row
+    used.update(board[row])
+
+    # Column
+    for r in range(9):
+        used.add(board[r][col])
+
+    # Box
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+    for r in range(box_row, box_row + 3):
+        for c in range(box_col, box_col + 3):
+            used.add(board[r][c])
+
+    candidates = get_digist() - used
+    return candidates
+
+
+def find_best_empty_cell(board) -> Optional[Cell]:
+    best: Optional[Cell] = None
+    min_candidates = 10
+
+    for row in range(9):
+        for col in range(9):
+            if is_cell_empty(board[row][col]):
+                cell = Cell(row, col)
+                candidates = get_candidates(board, cell)
+                count = len(candidates)
+
+                if count < min_candidates:
+                    min_candidates = count
+                    best = cell
+
+    return best
+
+
+def solve_sudoku(board: Board):
+    empty_cell = find_best_empty_cell(board)
+    if not empty_cell:
+        return True
+
+    for digit in get_candidates(board, empty_cell):
+        row = empty_cell.row
+        col = empty_cell.col
+        board[row][col] = digit
+
+        if solve_sudoku(board):
+            return True
+
+        board[row][col] = "."
+
+    return False
 
 
 def main():
     board = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], [
         "4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
-    solveSudoku(board)
-    print(board)
+    solve_sudoku(board)
 
 
 if __name__ == "__main__":
--- tests/test_solve_sudoku.py
+++ tests/test_solve_sudoku.py
@@ -1,13 +1,12 @@
 import unittest
 from parameterized import parameterized
 
-from solutions.solve_sudoku import solveSudoku
+from solutions.solve_sudoku import solve_sudoku
 from solutions.valid_sudoku import Solution as SudokuValidator
 
 
 BOARD_1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8",
                                                                                                                                                                                                         ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
-BOARD_2 = []
 
 
 class TestSolveSudoku(unittest.TestCase):
@@ -17,17 +16,14 @@ class TestSolveSudoku(unittest.TestCase):
 
     @parameterized.expand([
         (BOARD_1,),
-        (BOARD_2,),
     ])
     def test_given_valid_board_then_solve(self, board: list[list[str]]):
-        solveSudoku(board)
+        solve_sudoku(board)
 
-        # board is filled
         for row in board:
             for cell in row:
                 self.assertTrue(cell.isdigit())
 
-        # board is valid
         is_valid_board = self.validator.isValidSudoku(board)
         self.assertTrue(is_valid_board)
 
